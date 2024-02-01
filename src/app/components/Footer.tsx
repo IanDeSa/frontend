149407118159e517c5130299b7449583edd7708a
Ian Ecodots <ian@ecodots.com.br>
2024-02-01 01:51:41 -0400

    feat: Adicionar componentes Header e Footer
    Cria os componentes Header e Footer para fornecer uma navegação consistente e um layout global para a aplicação.

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="bg-sky-800 text-white fixed bottom-0 left-77 w-full h-35px">
+      Meu Footer
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="bg-sky-800 text-white fixed top-0 left-77 w-full p-4">
+      Meu Header
+    </header>
+  )
+}
+
+export default Header;
